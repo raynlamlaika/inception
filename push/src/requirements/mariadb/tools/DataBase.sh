4d7ef4d3d4ec6748c7c35d19da0de901b33b9fe9
lamlaika <lamlaikarayane@gmail.com>
2026-05-31 12:19:04 +0100

    add the docker-compose

---
+++ push/src/requirements/mariadb/tools/DataBase.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# here the  setup that we will need to set up the database and run it when the container starts is defined.
+
+# first we will start the mariadb service temporarily to initialize the database and create the necessary user and database if they do not exist.
+
+if [ -f /run/secrets/db_root_password ]; then
+    MYSQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+fi
+
+if [ -f /run/secrets/db_password ]; then
+    MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+fi
+
+MYSQL_DATABASE=${MYSQL_DATABASE:-wordpress}
+MYSQL_USER=${MYSQL_USER:-wordpress}
+
+# service mariadb start is used to start the mariadb service in the container. This allows us to run the mysql commands to set up the database and user.
+service mariadb start
+
+until mysqladmin -u root ping > /dev/null 2>&1 || mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} ping > /dev/null 2>&1; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+
+
+
+if mysql -u root -e "SELECT 1;" > /dev/null 2>&1; then
+    # if the root user does not have a password, we will set it up and create the database and user.
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;" # the flush privileges command is used to reload the privileges from the grant tables in the mysql database. This is necessary after making changes to the user accounts or permissions to ensure that the changes take effect immediately.
+else
+    # if the root user already has a password, we will use it to create the database and user.
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%';"
+    mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
+fi
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown # after setting up the database and user, we will shut down the mariadb service to start it again in the foreground.
+
+
+# finally, we will start the mariadb service in the foreground so that it continues to run and accept connections from other containers.
+echo "MariaDB is ready. Starting in foreground..."
+exec mariadbd-safe --bind-address=0.0.0.0 # the exec command is used to replace the current shell process with the mariadbd-safe process. This allows the container to run the mariadb service in the foreground and keep it running as long as the container is running. The --bind-address=
+
+
+
+
+
+
+
---
+++ push/src/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+if [ -f /run/secrets/db_password ]; then
+    MYSQL_PASSWORD=$(cat /run/secrets/db_password)
+fi
+
+if [ -f /run/secrets/wp_admin_password ]; then
+    WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+fi
+
+if [ -f /run/secrets/wp_password ]; then
+    WP_USER_PASSWORD=$(cat /run/secrets/wp_password)
+fi
+
+#waiting for the database to be ready before starting wordpress setup
+for i in {1..30}; do 
+    if mariadb -h mariadb -u $MYSQL_USER -p$MYSQL_PASSWORD -e "SELECT 1" >/dev/null 2>&1; then break
+    fi
+        echo "[$i/30] ... waiting for mariadb"
+        sleep 2
+done
+echo "mariadb is ready!"
+
+# if the wp-config.php file does not exist, we will set up wordpress. This is done to ensure that we only set up wordpress once and not every time the container starts.
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp core --allow-root download --force
+    wp config --allow-root create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost="mariadb"
+    chown -R www-data:www-data /var/www/html
+    chmod 755 /var/www/html
+fi
+
+if ! wp core --allow-root is-installed; then
+    wp core --allow-root install --title="${WP_TITLE}" --url="${DOMAIN_NAME}" --admin_user="${WP_ADMIN_USER}" --admin_password="${WP_ADMIN_PASSWORD}" --admin_email="${WP_ADMIN_EMAIL}" --skip-email
+    wp user --allow-root create "${WP_USER}" "${WP_USER_EMAIL}" --user_pass="${WP_USER_PASSWORD}" --role=author
+fi
+
+wp option update home "${DOMAIN_NAME}" --allow-root
+wp option update siteurl "${DOMAIN_NAME}" --allow-root
+
+if [ "${DOMAIN_NAME}" != "https://127.0.0.1:8443/" ]; then
+    wp option update home "https://127.0.0.1:8443/" --allow-root
+    wp option update siteurl "https://127.0.0.1:8443/" --allow-root
+fi
+
+
+sed -i 's/listen = .*/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
+mkdir -p /run/php #check why
+
+
+echo "wordpress is ready!"
+
+exec php-fpm7.4 -F
+
